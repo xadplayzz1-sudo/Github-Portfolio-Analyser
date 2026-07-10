b25b17c490b37d61b0cb5f4c32034b912228ccc5
xADplayzz <xadplayzz1@gmail.com>
2026-07-10 19:03:32 +0100

    Fixed importing of module useState into React Server Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,34 @@
-// React calls home function to display the page + return means return the user interface
+import { useState } from "react";
+import Header from "../components/Header";
+
 export default function Home() {
+
+  const [username, setUsername] = useState("");
+
   return (
     <main className="min-h-screen bg-slate-100 flex flex-col items-center py-20 px-6">
-      <h1 className="text-5xl font-bold text-slate-900">
-        GitHub Portfolio Analyser
-      </h1>
 
-      <p className="mt-4 text-lg text-slate-600 text-center max-w-2xl">
-        Analyses Github profiles to create capability insights based on evidence from Github API
-      </p>
+      <Header />
 
       <div className="mt-10 flex gap-4 w-full max-w-xl">
         <input
           type="text"
           placeholder="Enter GitHub username..."
-          className="flex-1 rounded-lg border border-slate-300 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          className="flex-1 rounded-lg border border-slate-300 px-4 py-3 text-slate-900 placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
 
-        <button className="rounded-lg bg-blue-600 px-6 py-3 font-semibold text-white hover:bg-blue-700 transition">
-          Analyse 
-        </button> 
+        <button
+          onClick={() => console.log(username)}
+          className="rounded-lg bg-blue-600 px-6 py-3 font-semibold text-white hover:bg-blue-700 transition"
+        >
+         Analyse
+        </button>
       </div>
     </main>
   );
 }
+// React calls home function to display the page + return means return the user interface
 // analyse button for future is <button>
 // <input /> for text input for user - need to make it work to remember what user types (in this case the Github username)
