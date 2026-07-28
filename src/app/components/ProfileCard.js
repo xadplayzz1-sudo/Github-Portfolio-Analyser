60fd47994cc0c364928aea8f2605396205980c9e
xADplayzz <xadplayzz1@gmail.com>
2026-07-28 01:28:49 +0100

    Fixed profile card not being in Dark mode

--- src/app/components/ProfileCard.js
+++ src/app/components/ProfileCard.js
@@ -2,9 +2,10 @@ export default function ProfileCard({ profile, theme }) {
     if (!profile) return null;
 
     const isDark = theme === "dark";
-
+    console.log("Theme:", theme);
+    console.log("isDark:", isDark);
     return (
-        <div className={`card float-in rounded-[28px] border p-5 transition duration-300 hover:-translate-y-0.5 ${isDark ? "border-[#30363d] bg-[linear-gradient(180deg,#161b22_0%,#0d1117_100%)] shadow-[0_12px_30px_rgba(0,0,0,0.35)] hover:shadow-[0_16px_34px_rgba(0,0,0,0.45)]" : "border-[#d0d7de] bg-[linear-gradient(180deg,#ffffff_0%,#f6f8fa_100%)] shadow-[0_12px_30px_rgba(31,35,40,0.08)] hover:shadow-[0_16px_34px_rgba(31,35,40,0.12)]"}`}>
+        <div className={`float-in rounded-[28px] border p-5 transition duration-300 hover:-translate-y-0.5 ${isDark ? "border-[#30363d] bg-[linear-gradient(180deg,#161b22_0%,#0d1117_100%)] shadow-[0_12px_30px_rgba(0,0,0,0.35)] hover:shadow-[0_16px_34px_rgba(0,0,0,0.45)]" : "border-[#d0d7de] bg-[linear-gradient(180deg,#ffffff_0%,#f6f8fa_100%)] shadow-[0_12px_30px_rgba(31,35,40,0.08)] hover:shadow-[0_16px_34px_rgba(31,35,40,0.12)]"}`}>
             <div className="flex flex-col gap-6 sm:flex-row sm:items-center">
                 <img
                     src={profile.avatar_url}
